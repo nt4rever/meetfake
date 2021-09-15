9ab5cac818bb6fa731a34d6ed38a32c8defe6d3b
nt4rever <levantanald@gmail.com>
2021-09-15 13:22:35 +0700

    data sources

---
+++ src/main/java/com/example/meetfake/config/ConfigDataSource.java
@@ -0,0 +1,54 @@
+package com.example.meetfake.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+public class ConfigDataSource {
+	private String url = "jdbc:mysql://n20ugxjbjyo2yiy5:lowuhb620xx3m1fg@dcrhg4kh56j13bnu.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/hai24bipahfkg8fz";
+	private String user = "n20ugxjbjyo2yiy5";
+	private String password = "lowuhb620xx3m1fg";
+	private String driverClass = "com.mysql.cj.jdbc.Driver";
+
+	@Bean(name = "dataSource")
+	public DataSource dataSource() {
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setJdbcUrl(url);
+		hikariConfig.setDriverClassName(driverClass);
+		hikariConfig.setUsername(user);
+		hikariConfig.setPassword(password);
+		HikariDataSource hikariDataSource = new HikariDataSource(hikariConfig);
+		return hikariDataSource;
+	}
+
+	@Bean(name = "transactionManager")
+	public DataSourceTransactionManager dataSourceTransactionManager() {
+		return new DataSourceTransactionManager(dataSource());
+	}
+
+	@Bean(name = "sqlSessionFactory")
+	public SqlSessionFactory sqlSessionFactory(@Qualifier("dataSource") DataSource dataSource) throws Exception {
+		final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource);
+
+		/* Set the mapper file location */
+		sessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver()
+				.getResources("classpath:com/example/meetfake/mapper/sql/*xml"));
+
+		/* Set entity class mapping rules: Underscore -> Hump */
+		org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration();
+		configuration.setMapUnderscoreToCamelCase(true);
+		sessionFactory.setConfiguration(configuration);
+		return sessionFactory.getObject();
+	}
+}
